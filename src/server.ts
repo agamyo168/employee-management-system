bf2d17d3528ac99cbed642a71d378d0c4ae0063b
agamyo168 <agamyo168@gmail.com>
2025-01-22 17:36:19 +0200

    add errors and notfound middleware

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,7 @@ import express from 'express';
 import sequelize from './configs/db-connection.config';
 import logger from './helpers/logger';
 import { StatusCodes } from 'http-status-codes';
-import Departments from './models/departments.model';
-import Employees from './models/employees.model';
+import notFoundMiddleware from './middlewares/notfound.middleware';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -14,17 +13,20 @@ app.get('/api/v1/healthcheck', (req, res, next) => {
   res.status(StatusCodes.OK).json({ success: true, message: 'Health check!' });
 });
 
+// End of stack middlewares
+//Not Found middleware
+app.use(notFoundMiddleware);
+// Error Handling middleware
 const start = async () => {
   try {
     await sequelize.sync(/*{ force: true }*/);
-
+    logger.info('DB Connected');
     app.listen(port, () => {
-      console.log(`Server is listening on http://localhost:${port}`);
+      logger.info(`Server is listening on http://localhost:${port}`);
     });
   } catch (err) {
     logger.error(err);
   }
 };
 start();
-
 export default app;
--- src/tests/server.spec.ts
+++ src/tests/server.spec.ts
@@ -10,3 +10,10 @@ describe('Healthcheck', () => {
     expect(res.status).toBe(StatusCodes.OK);
   });
 });
+
+describe('Testing Not Found Middleware', () => {
+  it(`should return ${StatusCodes.NOT_FOUND} when accessing a not handled url`, async () => {
+    const res = await request.get('/invalid-url');
+    expect(res.status).toBe(StatusCodes.NOT_FOUND);
+  });
+});
