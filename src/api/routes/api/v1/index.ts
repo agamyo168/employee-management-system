18b44fabaf923991d47fef82ad5d51ceb7f9c949
agamyo168 <agamyo168@gmail.com>
2025-01-23 00:33:30 +0200

    add authHandlerMiddleware

--- src/api/routes/api/v1/index.ts
+++ src/api/routes/api/v1/index.ts
@@ -2,10 +2,11 @@ import express from 'express';
 import authRoute from './auth.route';
 import employeeRoute from './employees.route';
 import departmentRoute from './departments.route';
+import authHandlerMiddleware from '../../../../middlewares/auth-handler.middleware';
 
 const router = express.Router();
 router.use('/auth', authRoute);
-router.use('/employees', employeeRoute);
-router.use('/departments', departmentRoute);
+router.use('/employees', authHandlerMiddleware, employeeRoute);
+router.use('/departments', authHandlerMiddleware, departmentRoute);
 
 export default router;
---
+++ src/middlewares/auth-handler.middleware.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import UnauthorizedError from '../errors/custom/unauthorized.error.class';
+import logger from '../helpers/logger';
+
+const authHandlerMiddleware = (
+  _req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const authHead = _req.headers.authorization;
+  if (authHead == null || authHead.startsWith('Bearer') == false)
+    return next(new UnauthorizedError(`invalid user token`));
+
+  const token = authHead.split(' ')[1];
+  try {
+    const payload = jwt.verify(token, String(process.env.JWT_SECRET));
+    res.locals.payload = payload;
+    next();
+  } catch (err) {
+    logger.error(err);
+    return next(new UnauthorizedError(`invalid user token`));
+  }
+};
+
+export default authHandlerMiddleware;
