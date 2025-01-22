f2bdf26c04e0167fe4e83e907989b0a5ef6a8ff0
agamyo168 <agamyo168@gmail.com>
2025-01-22 23:19:37 +0200

    add employees route

--- src/api/controllers/employees.controller.ts
+++ src/api/controllers/employees.controller.ts
@@ -1,16 +1,86 @@
 import { Request, Response, NextFunction } from 'express';
-import { EmployeeParams } from '../../types/employees/employees.interface';
 import Employees from '../../models/employees.model';
 import logger from '../../helpers/logger';
 import BadRequestError from '../../errors/custom/bad.request.error.class';
-const addEmployee = async (req: Request, res: Request, next: NextFunction) => {
+import { StatusCodes } from 'http-status-codes';
+import NotFound from '../../errors/custom/notfound.error.class';
+const addEmployee = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const employee = await Employees.create(req.body);
+    res.status(StatusCodes.CREATED).json({ success: true, employee });
   } catch (error) {
     logger.error(error);
     return next(new BadRequestError('Failed to add employee')); // Email already exists
   }
 };
-const getAllEmployees = () => {};
+const getAllEmployees = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const employees = await Employees.findAll();
+    res.status(StatusCodes.OK).json({ success: true, employees });
+  } catch (error) {
+    logger.error(error);
+    return next(new Error('Failed to get all employees')); //Internal server error
+  }
+};
+const getEmployeeById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const employee = await Employees.findByPk(req.params.id);
+    if (!employee) {
+      return next(new NotFound('Employee not found'));
+    }
+    res.status(StatusCodes.OK).json({ success: true, employee });
+  } catch (error) {
+    logger.error(error);
+    return next(new Error('Failed to get employee by ID'));
+  }
+};
+const updateEmployee = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const employee = await Employees.findByPk(req.params.id);
+    if (!employee) {
+      return next(new NotFound('Employee not found'));
+    }
+    await employee.update(req.body);
+    res.status(StatusCodes.OK).json({ success: true, employee });
+  } catch (error) {
+    logger.error(error);
+    return next(new BadRequestError('Failed to update employee'));
+  }
+};
+const deleteEmployee = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const employee = await Employees.findByPk(req.params.id);
+    if (!employee) {
+      return next(new NotFound('Employee not found'));
+    }
+    await employee.destroy();
+    res.status(StatusCodes.NO_CONTENT).json({ success: true });
+  } catch (error) {
+    logger.error(error);
+    return next(new Error('Failed to delete employee')); // Or Internal Server Error
+  }
+};
 
-export { addEmployee, getAllEmployees };
+export {
+  getEmployeeById,
+  updateEmployee,
+  deleteEmployee,
+  getAllEmployees,
+  addEmployee,
+};
--- src/api/routes/api/v1/employees.route.ts
+++ src/api/routes/api/v1/employees.route.ts
@@ -1,5 +1,17 @@
 import express from 'express';
+import {
+  addEmployee,
+  deleteEmployee,
+  getAllEmployees,
+  getEmployeeById,
+  updateEmployee,
+} from '../../../controllers/employees.controller';
 
 const router = express.Router();
-router.route('/').get().post();
+router.route('/').get(getAllEmployees).post(addEmployee);
+router
+  .route('/:id')
+  .get(getEmployeeById)
+  .patch(updateEmployee)
+  .delete(deleteEmployee);
 export default router;
